ca1a48acff94af60ebc42731533e3584d6371d68
LilianMar <lilian.maradiago@gmail.com>
2024-08-13 20:31:16 -0500

    initial commit-SOA aplication1

---
+++ src/main/java/co/com/Maradiago/Lilian/LilianApplication.java
@@ -0,0 +1,13 @@
+package co.com.Maradiago.Lilian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LilianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/Maradiago/Lilian/LilianApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.Maradiago.Lilian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
